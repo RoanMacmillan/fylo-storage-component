f75269fe5165178086b68a83cd04db000d8cc717
RoanMacmillan <hibee94@gmail.com>
2023-02-01 14:54:20 +0000

    small fix

--- src/components/BottomContainer.jsx
+++ src/components/BottomContainer.jsx
@@ -4,7 +4,7 @@ import Bar from './bar/Bar';
 
 const BottomContainer = () => (
     <div className='bottom-container'>
-      <p className='usage-text'>You've used<span className='usage-amnt'> 815 GB</span> of your storage</p>
+      <h1 className='usage-text'>You've used<span className='usage-amnt'> 815 GB</span> of your storage</h1>
       <Bar />
       <div className='gb-container'>
         <span className='left'>0 GB</span>
